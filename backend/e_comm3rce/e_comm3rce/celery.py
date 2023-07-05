c8251020c8e17e64d0d7334c08026e0390053001
just4ndy <just4ndy@github.com>
2023-07-05 12:21:39 +0300

    send_mails is now scheduled

--- backend/e_comm3rce/e_comm3rce/celery.py
+++ backend/e_comm3rce/e_comm3rce/celery.py
@@ -1,6 +1,8 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
+from django.conf import settings
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'e_comm3rce.settings')
@@ -15,3 +17,11 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+app.conf.update(timezone=settings.TIME_ZONE)
+app.conf.beat_schedule = {
+    'send-every-day-noon': {
+        'task': 'waitings.tasks.send_mails_for_waiting_users',
+        'schedule': crontab(hour='12', minute='21'),
+    },
+}
--- backend/e_comm3rce/e_comm3rce/settings.py
+++ backend/e_comm3rce/e_comm3rce/settings.py
@@ -125,7 +125,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Kiev'
+TIME_ZONE = env.str('TIME_ZONE')
 
 USE_I18N = True
 
