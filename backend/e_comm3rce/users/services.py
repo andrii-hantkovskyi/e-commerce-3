ea552ba7b7fc3e25aa2ebc69f281808cb21f8efb
just4ndy <just4ndy@github.com>
2023-07-05 21:40:28 +0300

    added customer info update

--- backend/e_comm3rce/users/services.py
+++ backend/e_comm3rce/users/services.py
@@ -17,9 +17,27 @@ def _create_customer(user: User, first_name: str, last_name: str, phone: str) ->
     return customer
 
 
-def register_new_customer(email: str, password: str, first_name: str, last_name: str, phone: str) -> Customer:
+def register_new_customer(email: str, password: str, first_name: str, last_name: str, phone: str) -> Customer | None:
+    if not all((email, password, first_name, last_name, phone)):
+        raise ValueError('Not all fields provided')
     user = _create_user(email=email, password=password)
     customer = _create_customer(user=user, first_name=first_name, last_name=last_name, phone=phone)
     create_waiting_list(customer)
     create_favorite_list(customer)
+    return customer
+
+
+def update_customer_info(customer: Customer, email, first_name, last_name, phone) -> Customer:
+    if email and customer.user.email != email:
+        customer.user.email = email
+    if first_name and customer.first_name != first_name:
+        customer.first_name = first_name
+    if last_name and customer.last_name != last_name:
+        customer.last_name = last_name
+    if phone and customer.phone_number != phone:
+        customer.phone_number = phone
+
+    if any((email, first_name, last_name, phone)):
+        customer.save()
+
     return customer
--- backend/e_comm3rce/users/viewsets.py
+++ backend/e_comm3rce/users/viewsets.py
@@ -1,28 +1,46 @@
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin
+from rest_framework.decorators import action
+from rest_framework.mixins import CreateModelMixin
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST, HTTP_200_OK
 from rest_framework.viewsets import GenericViewSet
 
 from users.models import Customer
 from users.permissions import IsAuthenticatedOrCreateOnly
 from users.serializers import CustomerSerializer
-from users.services import register_new_customer
+from users.services import register_new_customer, update_customer_info
 
 
 class CustomerViewSet(CreateModelMixin,
-                      RetrieveModelMixin,
-                      UpdateModelMixin,
                       GenericViewSet):
     serializer_class = CustomerSerializer
     queryset = Customer.objects.all()
     permission_classes = [IsAuthenticatedOrCreateOnly]
 
     def create(self, request, *args, **kwargs):
-        email, password, first_name, last_name, phone = request.data.get('email'), request.data.get(
-            'password'), request.data.get('first_name'), request.data.get('last_name'), request.data.get('phone')
-
         try:
+            email, password, first_name, last_name, phone = request.data.get('email'), request.data.get(
+                'password'), request.data.get('first_name'), request.data.get('last_name'), request.data.get('phone')
+
             register_new_customer(email, password, first_name, last_name, phone)
+
             return Response(status=HTTP_201_CREATED)
+        except Exception as ex:
+            return Response(status=HTTP_400_BAD_REQUEST, data=ex)
+
+    @action(detail=False, methods=['GET'], url_path='get-profile')
+    def get_profile(self, request, *args, **kwargs):
+        serialized_data = self.get_serializer(request.user.customer).data
+        return Response(status=HTTP_200_OK, data=serialized_data)
+
+    @action(detail=False, methods=['PUT', 'PATCH'], url_path='update-profile')
+    def update_profile(self, request, *args, **kwargs):
+        try:
+            email, first_name, last_name, phone = request.data.get('email'), request.data.get(
+                'first_name'), request.data.get('last_name'), request.data.get('phone')
+
+            customer = update_customer_info(request.user.customer, email, first_name, last_name, phone)
+            serialized_data = self.get_serializer(customer).data
+
+            return Response(status=HTTP_200_OK, data=serialized_data)
         except Exception as ex:
             return Response(status=HTTP_400_BAD_REQUEST, data=ex)
